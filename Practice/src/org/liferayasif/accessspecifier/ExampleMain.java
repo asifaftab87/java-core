aeeb6fd3c39717352a63ed9644568e05f2830fde
MuhammadShahbaz <mdshahbazengg@gmail.com>
2018-10-08 19:21:37 +0530

    Second commit but new file from shahbaz

---
+++ Practice/src/org/liferayasif/accessspecifier/ExampleMain.java
@@ -0,0 +1,11 @@
+package org.liferayasif.accessspecifier;
+
+public class ExampleMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("first class to commit by shahbaz");
+
+	}
+
+}
